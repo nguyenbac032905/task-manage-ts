2e620b10e4fb05ff3af795ef3ddf210e8963d767
nguyen van bac <nguyenbac29305@gmail.com>
2026-02-21 20:43:49 +0700

    cai dat cors

--- index.ts
+++ index.ts
@@ -12,8 +12,12 @@ const port: number | string = process.env.PORT || 3000;
 import bodyParser from "body-parser";
 app.use(bodyParser.json());
 
+import cors from "cors";
+app.use(cors());
+
 import routes from "./routes/index.route";
 routes(app);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
