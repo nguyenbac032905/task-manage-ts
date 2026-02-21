580928f80cad6340902a35ffbd3adead92e67389
nguyen van bac <nguyenbac29305@gmail.com>
2026-02-21 14:45:38 +0700

    loc theo trang thai va sap xep

--- controllers/task.controller.ts
+++ controllers/task.controller.ts
@@ -1,7 +1,25 @@
 import {Request, Response} from "express";
 import Task from "../models/task.model";
 export const index = async (req: Request,res: Response): Promise<void> => {
-    const tasks = await Task.find({deleted: false});
+    interface Find {
+        deleted: boolean;
+        status?: string;
+    }
+    let find: Find = {
+        deleted: false
+    };
+
+    const status = req.query.status as string;
+    if(status){
+        find.status = status;
+    }
+    const sort = {};
+    const sortKey = req.query.sortKey as string;
+    const sortValue = req.query.sortValue as string;
+    if(sortKey && sortValue){
+        sort[sortKey] = sortValue;
+    }
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks);
 }
 export const detail = async (req: Request, res: Response): Promise<void> => {
